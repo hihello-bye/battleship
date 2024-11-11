ef8f119a21c173935fb14827b1d00fc6b9502608
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-11-11 17:56:55 +0000

    Check ship is not initially sunk

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,8 +1,14 @@
 const Ship = require('../ship.js');
 
 describe('Ship', () => {
+
     test('should initialize with the correct length', () => {
         const ship = Ship(3);
         expect(ship.length).toBe(3);
+    })
+
+    test('Should not initially be sunk', () => {
+        const ship = Ship(3);
+        expect(ship.isSunk()).toBe(false);
     })
 })
