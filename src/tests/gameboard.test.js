565083402696a1c195b614c9304aa624d7cffbd4
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-11-12 14:07:12 +0000

    Fix test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -2,11 +2,14 @@ const Gameboard = require('../gameboard.js');
 const Ship = require('../ship.js');
 
 describe('Gameboard', () => {
-    const gameboard = Gameboard();
-    const ship = Ship(3);
-    gameboard.placeShip(ship, [0, 0], "horizontal");
-
-    expect(gameboard.getShips()).toEqual([
-        { ship, coordinates: [[0,0], [0,1], [0,2]] }
-    ])
+    test('should place a ship at specified coordinates', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(3);
+        gameboard.placeShip(ship, [0, 0], "horizontal");
+    
+        expect(gameboard.getShips()).toEqual([
+            { ship, coordinates: [[0,0], [0,1], [0,2]] }
+        ])
+    })
+   
 })
