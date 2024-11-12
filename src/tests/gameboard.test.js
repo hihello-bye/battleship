37cff263a85bfff8400b0a14e8469905ac5233e4
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-11-12 14:21:04 +0000

    Check receiveAttack
    See if registers a hit on a ship at specified coordinates,
    and tracks missed shots

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -2,6 +2,7 @@ const Gameboard = require('../gameboard.js');
 const Ship = require('../ship.js');
 
 describe('Gameboard', () => {
+
     test('should place a ship at specified coordinates', () => {
         const gameboard = Gameboard();
         const ship = Ship(3);
@@ -12,4 +13,13 @@ describe('Gameboard', () => {
         ])
     })
    
+    test('should register a hit on a ship at specified coordinates and track missed shots', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(3);
+        gameboard.placeShip(ship, [0, 0], 'horizontal');
+
+        gameboard.recieveAttack([0,1]);
+        expect(ship.sunk()).toBe(false);
+        expect(gameboard.getMissedShots()).toEqual([]);
+    })
 })
